6a22cae380a3f140b8a90b4e58c5f80ec088eda2
danirusz <103048819+danirusz@users.noreply.github.com>
2023-08-07 15:28:59 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
-import streamlit 
+import streamlit
+
 streamlit.title('My parents new healthy diner')
+
 streamlit.header.('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
